9c4a7db1b51ca66c693e6017488f437ca92b0369
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-11-02 22:32:52 +0100

    `ultralytics 8.0.205` HUB dataset downloads fix (#6101)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -639,6 +639,23 @@ def colorstr(*input):
     return ''.join(colors[x] for x in args) + f'{string}' + colors['end']
 
 
+def remove_colorstr(input_string):
+    """
+    Removes ANSI escape codes from a string, effectively un-coloring it.
+
+    Args:
+        input_string (str): The string to remove color and style from.
+
+    Returns:
+        (str): A new string with all ANSI escape codes removed.
+
+    Examples:
+        >>> remove_colorstr(colorstr('blue', 'bold', 'hello world'))
+        >>> 'hello world'
+    """
+    ansi_escape = re.compile(r'\x1B\[[0-9;]*[A-Za-z]')
+    return ansi_escape.sub('', input_string)
+
 class TryExcept(contextlib.ContextDecorator):
     """YOLOv8 TryExcept class. Usage: @TryExcept() decorator or 'with TryExcept():' context manager."""
 
